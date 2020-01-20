88918abbac85fb4fa0530e5219ab8d3146a1fe2f
Alind Khare <alindkhare@gatech.edu>
2020-01-20 08:50:54 -0500

    check

--- ensemble_profiler/store_data_actor.py
+++ ensemble_profiler/store_data_actor.py
@@ -23,7 +23,8 @@ class StatefulPatientActor:
         self.patient_data = defaultdict(list)
         # value_type: ECG (supported right now), vitals etc.
         self.supported_vtypes = supported_vtype
-        print("ACTOR STARTED: {}".format(self.patient_name))
+        print("ACTOR STARTED: {}, {}".format(
+            self.patient_name, self.patient_data))
 
     def get_periodic_predictions(self, info):
         # print(self.pipeline)
@@ -36,8 +37,8 @@ class StatefulPatientActor:
         if value_type == self.supported_vtypes:
             result = "Data Recorded"
             # append the data point to the patient's stored data structure
-            patient_val_list = self.patient_data[value_type]
-            patient_val_list.append(torch.tensor([[value]]))
+            # patient_val_list = self.patient_data[value_type]
+            # patient_val_list.append(torch.tensor([[value]]))
             # if len(patient_val_list) == self.periodic_interval:
             #     data = torch.cat(patient_val_list, dim=1)
             #     data = torch.stack([data])
