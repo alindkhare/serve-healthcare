0ba2d5a2ac6eba8dea9843ab084f971a372ac2f6
Alind Khare <alindkhare@gatech.edu>
2020-01-23 21:31:31 -0500

    checking out logging info

--- ensemble_profiler/server.py
+++ ensemble_profiler/server.py
@@ -190,5 +190,5 @@ class HTTPActor:
 
     def run(self, host="0.0.0.0", port=5000):
         uvicorn.run(
-            self.app, host=host, port=port, lifespan="on", access_log=False,
+            self.app, host=host, port=port, lifespan="on", access_log=True,
             limit_concurrency=sys.maxsize, limit_max_requests=sys.maxsize)
