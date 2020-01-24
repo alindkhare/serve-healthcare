1daffca65d7960794a3c1e1ff7a4c1f0d3223283
Alind Khare <alindkhare@gatech.edu>
2020-01-24 12:59:11 -0500

    Added wait for http server to be hosted

--- ensemble_profiler/server.py
+++ ensemble_profiler/server.py
@@ -191,6 +191,6 @@ class HTTPActor:
 
     def run(self, host="0.0.0.0", port=5000):
         uvicorn.run(
-            self.app, host=host, port=port, lifespan="on", access_log=True,
+            self.app, host=host, port=port, lifespan="on", access_log=False,
             limit_concurrency=sys.maxsize, limit_max_requests=sys.maxsize,
             timeout_keep_alive=5)
