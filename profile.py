01949bb1a2f607bfd81d11de0838810ac5de7bdf
Alind Khare <alindkhare@gatech.edu>
2020-02-05 23:10:15 -0500

    check

--- profile.py
+++ profile.py
@@ -5,24 +5,24 @@ from pathlib import Path
 import ray.experimental.serve as serve
 import ray
 
-# ECG
-n_channel = 1
-base_filters = 64
-kernel_size = 16
-n_classes = 2
-n_block = 2
-model = ResNet1D(in_channels=n_channel,
-                 base_filters=base_filters,
-                 kernel_size=kernel_size,
-                 stride=2,
-                 n_block=n_block,
-                 groups=base_filters,
-                 n_classes=n_classes,
-                 downsample_gap=max(n_block//8, 1),
-                 increasefilter_gap=max(n_block//4, 1),
-                 verbose=False)
+# # ECG
+# n_channel = 1
+# base_filters = 64
+# kernel_size = 16
+# n_classes = 2
+# n_block = 2
+# model = ResNet1D(in_channels=n_channel,
+#                  base_filters=base_filters,
+#                  kernel_size=kernel_size,
+#                  stride=2,
+#                  n_block=n_block,
+#                  groups=base_filters,
+#                  n_classes=n_classes,
+#                  downsample_gap=max(n_block//8, 1),
+#                  increasefilter_gap=max(n_block//4, 1),
+#                  verbose=False)
 print("yooooooo")
-filename = "profile_results.jsonl"
-file_path = Path(filename)
-ans = profiler.profile_ensemble([model], file_path, fire_clients=False)
-print("Answer is : {}".format(ans))
+# filename = "profile_results.jsonl"
+# file_path = Path(filename)
+# ans = profiler.profile_ensemble([model], file_path, fire_clients=False)
+# print("Answer is : {}".format(ans))
